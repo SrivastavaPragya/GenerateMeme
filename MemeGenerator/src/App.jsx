b249bae3df52c373b40a0377bcc1b6a9fb252edf
Pragya <pragya142018@gmail.com>
2023-12-17 00:23:51 +0530

    first commit

---
+++ MemeGenerator/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './components/Header'
+import Meme from './components/Meme'
+import '../src/App.css'
+
+const App = () => {
+  return (
+    <div>
+     <Header/>
+     <Meme/>
+    </div>
+  )
+}
+
+export default App
+
